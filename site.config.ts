b2bb21dd8dc0f08948bda7976a58627cd8f9d274
NgDMau <maunguyen.dinh@gmail.com>
2023-07-03 00:27:51 +0900

    update config

--- site.config.ts
+++ site.config.ts
@@ -10,17 +10,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Mau Demo',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Mau DN',
+  name: 'NDM Personal Blog',
+  domain: 'demo.maudn.com',
+  author: 'Mau D. Nguyen',
 
   // open graph metadata (optional)
-  description: 'Mot blog don gian cua Thanh',
+  description: 'NDM Personal Blog',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'nddmau',
+  github: 'NgDMau',
+  linkedin: 'maudn',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
